8c1f1750f10a311c78689ebdbaaeb68aa5052b22
y.tomita-dev <tomita.yuji@tis.co.jp>
2019-07-27 05:15:17 +0000

    ナビゲーションバーの追加

---
+++ src/components/list.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+        <div>登録されている書籍を一覧表示するページ</div>
+    );
+  }
+}
+
+export default List;
---
+++ src/components/regist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Regist extends Component {
+  render() {
+    return (
+        <div>書籍を登録するページ</div>
+    );
+  }
+}
+
+export default Regist;

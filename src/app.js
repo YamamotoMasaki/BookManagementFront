67772d7c762a66452417570d4e8d69f1920d33f2
y.tomita-dev <tomita.yuji@tis.co.jp>
2019-07-27 15:46:33 +0000

    Routeに'/'を追加

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './components/navbar';
+import Home from './components/home';
 import List from './components/list';
 import Regist from './components/regist';
 import Loan from './components/loan';
@@ -26,6 +27,7 @@ class App extends Component {
           open={this.state.open}
         / >
         <Switch>
+            <Route path='/' exact component={Home}/>        
             <Route path='/list' exact component={List}/>
             <Route path='/regist' exact component={Regist}/>
             <Route path='/loan' exact component={Loan}/>
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+        <div>初期表示</div>
+    );
+  }
+}
+
+export default Home;

dd6f85aecf0523550d503e6a05c35035b6f3d8b3
r-kato <kattochan1996@yahoo.co.jp>
2019-09-10 07:43:14 +0000

    登録画面のチェック処理追加

--- src/pages/regist.js
+++ src/pages/regist.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout';
+import RegistApp from '../components/regist_comp/registApp';
 
 export default () => (
     <div>
         <Layout />
         <div>書籍を登録するページ</div>
+        <RegistApp />
     </div>
 )

bc4f368a3727c904ba5ef5f6d72ad927a2feda3c
Kato-R <kattochan1996@outlook.jp>
2019-10-01 05:41:36 +0000

    登録結果内容の表示テスト

---
+++ src/components/list_comp/listapp.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const classes = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
+
+class ListApp extends Component {
+    
+    constructor (props) {
+        super(props)
+        this.state = {
+            list_contents_info: this.getUrlParameter('bookname')
+        }
+    }
+    
+    //指定したパラメータのデータを取得する関数
+    getUrlParameter(name) {
+        name = name.replace(/[[]/, '[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        var results = regex.exec(window.location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    };
+    
+    render () {
+        if (!this.state.list_contents_info) {
+            return <div>Loading...</div>
+        }
+        return (
+            <div>
+                <Paper className={classes.root}>
+                  <Table className={classes.table}>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>{"書籍名"}</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                          <TableCell>{this.state.list_contents_info}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                  </Table>
+                </Paper>
+            </div>
+        )
+    }
+}
+
+export default ListApp
--- src/pages/registResult.js
+++ src/pages/registResult.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout';
+import ListApp from '../components/list_comp/listapp'
 
 export default () => (
     <div>
         <Layout />
         <div>登録結果</div>
+        <ListApp />
     </div>
 )
